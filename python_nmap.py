aafba56d0b6308640467cfcc98e1567e03e0ac6c
BesimAltnok <besimaltnok@gmail.com>
2015-09-22 17:47:44 +0300

    Create python_nmap.py

---
+++ python_nmap.py
@@ -0,0 +1,32 @@
+__author__ = 'besimaltnok'
+
+import nmap
+import sys
+
+nm = nmap.PortScanner()
+
+port_list = [21,22,25,139,3306, 1248]
+host = sys.argv[1]
+argument = '-n -T4 -O -sV -p '
+print "Scanning host : %s " %host
+print "\n"
+print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+print "Port\tName\tState\tReason"
+print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+try:
+
+	for i in port_list:
+		sc = nm.scan(hosts=host, arguments=argument + str(i))
+   		print "%s\t%s\t%s\t%s" %(i, nm[host]['tcp'][i]['name'] , nm[host]['tcp'][i]['state'], nm[host]['tcp'][i]['reason'])
+except KeyboardInterrupt:
+	print "\n"
+	print "!Scanning Stop!"
+	exit
+except NameError:
+	print "Stop!!!"
+	exit
+
+print "\n"
+print "Taranan Sistem Tipi :  %s " %(sc['scan'][host]['osmatch'][0]['name'])
+print "Tarama Zamani       : %s " % (sc["nmap"]['scanstats']['timestr'])
+print "Tarama Suresi       : %s " % (sc["nmap"]['scanstats']['elapsed'])
