b728080baa2fe249b375f340c233634b1ca789d2
BesimAltnok <besimaltnok@gmail.com>
2016-06-08 10:00:55 +0300

    Update nmap.py

--- examples/nmap.py
+++ examples/nmap.py
@@ -30,3 +30,51 @@ print "\n"
 print "Scanned System Type :  %s " %(sc['scan'][host]['osmatch'][0]['name'])
 print "Start scanning      : %s " % (sc["nmap"]['scanstats']['timestr'])
 print "Scan Time           : %s " % (sc["nmap"]['scanstats']['elapsed'])
+
+
+
+# Aktif bilgisayar tespiti
+
+import nmap
+nm = nmap.PortScanner()
+
+hostlist = ['ipler_buraya']
+for h in hostlist:
+   sc = nm.scan(h, '80', '-sV -O')
+   state = sc['nmap']['scanstats']['uphosts']
+   if state == '1':
+      print h, " : Aktif  host :)"
+   else:
+      print h, " : Kapali host :("
+
+
+# Aktif bilgisayarların mac tespiti
+
+
+import nmap
+nm = nmap.PortScanner()
+
+hostlist = ['ipler_buraya']
+for h in hostlist:
+   sc = nm.scan(h, '80', '-sV -O')
+   state = sc['nmap']['scanstats']['uphosts']
+   if state == '1':
+      mac = sc['scan']['addressses']['mac']
+      print mac, " : ", h
+      
+      
+      
+      
+# Aktif bilgisayarların işletim sistemi bilgisini elde etmek
+
+import nmap
+nm = nmap.PortScanner()
+
+hostlist = ['ipler_buraya']
+for h in hostlist:
+   sc = nm.scan(h, '80', '-sV -O')
+   state = sc['nmap']['scanstats']['uphosts']
+   if state == '1':
+      os  = sc['scan'][h]['osmatch'][1]['osclass'][0]['osfamily']
+      mac = sc['scan']['addressses']['mac']
+      print mac, " : ", h, " : ", os 
