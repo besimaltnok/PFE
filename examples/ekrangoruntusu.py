c2b6a9272d67eb7d9fb43b281627904fdc3b0182
Besim Altınok <besimaltnok@gmail.com>
2016-07-03 13:12:53 +0300

    Create ekrangoruntusu.py

---
+++ examples/ekrangoruntusu.py
@@ -0,0 +1,10 @@
+#brew install phantomjs
+#pip install selenium
+
+from selenium import webdriver
+import time
+driver = webdriver.PhantomJS()
+driver.set_window_size(1024, 768)
+driver.get('http://site')
+time.sleep(5)
+driver.save_screenshot('isim.png')
